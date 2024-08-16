6613a286545e8a444ebfea502b744b236e3adbff
Huanjia Liang <huanjial@uci.edu>
2024-08-15 21:11:02 -0700

    Added more integration test in CustomRepositoryImplIT.java to test the interaction between CustomRepositoryImpl.class and database.

--- src/test/java/com/example/movie_service/integration/CustomRepositoryImplIT.java
+++ src/test/java/com/example/movie_service/integration/CustomRepositoryImplIT.java
@@ -7,6 +7,8 @@ import com.example.movie_service.entity.Person;
 import com.example.movie_service.repository.CustomMovieRepositoryImpl;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,6 +61,11 @@ public class CustomRepositoryImplIT {
     }
 
     @BeforeEach
+    public void beforeEach() {
+        checkDatabaseEmpty();
+        initializeData();
+    }
+
     @Transactional
     public void checkDatabaseEmpty() {
         // Check if the database is empty
@@ -74,9 +81,8 @@ public class CustomRepositoryImplIT {
         entityManager.clear();
     }
 
-    @BeforeEach
     @Transactional
-    public void setUp() {
+    public void initializeData() {
         // Set up a Genre
         Genre genre = new Genre();
 //        genre.setId(1); Do not set an ID before you save/persist it. That's the only problem here. Hibernate looks at
@@ -87,27 +93,55 @@ public class CustomRepositoryImplIT {
         // Set up a director Person
         Person director = new Person();
         director.setName("Christopher Nolan");
-//        director.setId("nm1");
         entityManager.persist(director);
 
-        // Set up a Movie
-        Movie movie = new Movie();
-//        movie.setId("tt1");
-        movie.setTitle("The Dark Knight");
-        movie.setBackdropPath("backdropPath");
-        movie.setPosterPath("posterPath");
-        movie.setReleaseTime("2008-05-08");
-        entityManager.persist(movie);
+        // Set up a Movie 1
+        Movie movie1 = new Movie();
+        movie1.setTitle("The Dark Knight");
+        movie1.setReleaseTime("2008-05-08");
+        entityManager.persist(movie1);
+
+        // Set up a Movie 2
+        Movie movie2 = new Movie();
+        movie2.setTitle("The Dark Knight Rises");
+        movie2.setReleaseTime("2012-03-09");
+        entityManager.persist(movie2);
 
         // Add the genre to the movie
         Set<Genre> genres = new HashSet<>();
         genres.add(genre);
-        movie.setGenres(genres);
-        entityManager.persist(movie);
+        movie1.setGenres(genres);
+        movie2.setGenres(genres);
+        entityManager.persist(movie1);
+        entityManager.persist(movie2);
 
-        // Create a MovieCrew
-        MovieCrew movieCrew = new MovieCrew(movie, director, "director");
-        entityManager.persist(movieCrew);
+        // Set up MovieCrew
+        MovieCrew movieCrew1 = new MovieCrew(movie1, director, "director");
+        entityManager.persist(movieCrew1);
+        MovieCrew movieCrew2 = new MovieCrew(movie2, director, "director");
+        entityManager.persist(movieCrew2);
+
+        // Set up a Movie 3
+        Movie movie3 = new Movie();
+        movie3.setTitle("The Dark Knight Rises Again");
+        movie3.setReleaseTime("2012-08-09");
+        entityManager.persist(movie3);
+
+        // Set up Movie 3 genre, director, and movie crew
+        Genre genre2 = new Genre();
+        genre2.setName("Love");
+        entityManager.persist(genre2);
+        Set<Genre> genreSet2 = new HashSet<>();
+        genreSet2.add(genre2);
+        movie3.setGenres(genreSet2);
+        entityManager.persist(movie3);
+
+        Person director2 = new Person();
+        director2.setName("Third director");
+        entityManager.persist(director2);
+
+        MovieCrew movieCrew3 = new MovieCrew(movie3, director2, "director");
+        entityManager.persist(movieCrew3);
     }
 
     @Test
@@ -131,8 +165,53 @@ public class CustomRepositoryImplIT {
 
         assertNotNull(searchResults);
         assertTrue(searchResults.isEmpty());
-//        MovieSearchResultDTO firstResult = searchResults.get(0);
-//        assertEquals(firstResult.getTitle(), "The Dark Knight");
+    }
+
+    @Test
+    public void searchMovieTestAsc(){
+        List<MovieSearchResultDTO> searchResults = customMovieRepositoryImpl.searchMovies(
+                "Dark Knight", null, null, null, 10, 0,
+                "releaseTime", "asc");
+
+        assertNotNull(searchResults);
+        assertEquals(searchResults.size(), 3);
+        MovieSearchResultDTO firstResult = searchResults.get(0);
+        MovieSearchResultDTO secondResult = searchResults.get(1);
+        MovieSearchResultDTO thirdResult = searchResults.get(2);
+        assertEquals(firstResult.getTitle(), "The Dark Knight");
+        assertEquals(secondResult.getTitle(), "The Dark Knight Rises");
+        assertEquals(thirdResult.getTitle(), "The Dark Knight Rises Again");
+    }
+
+    @Test
+    public void searchMovieTestDesc(){
+        List<MovieSearchResultDTO> searchResults = customMovieRepositoryImpl.searchMovies(
+                "Dark Knight", null, null, null, 10, 0,
+                "releaseTime", "desc");
+
+        assertNotNull(searchResults);
+        assertEquals(searchResults.size(), 3);
+        MovieSearchResultDTO firstResult = searchResults.get(0);
+        MovieSearchResultDTO secondResult = searchResults.get(1);
+        MovieSearchResultDTO thirdResult = searchResults.get(2);
+        assertEquals(firstResult.getTitle(), "The Dark Knight Rises Again");
+        assertEquals(secondResult.getTitle(), "The Dark Knight Rises");
+        assertEquals(thirdResult.getTitle(), "The Dark Knight");
+    }
+
+    @Test
+    public void searchMovieByTitleAndDirector(){
+        List<MovieSearchResultDTO> searchResults = customMovieRepositoryImpl.searchMovies(
+                "Dark Knight", null, "Nolan", null, 10, 0,
+                "title", "asc");
+
+
+        assertNotNull(searchResults);
+        assertEquals(searchResults.size(), 2);
+        MovieSearchResultDTO firstResult = searchResults.get(0);
+        MovieSearchResultDTO secondResult = searchResults.get(1);
+        assertEquals(firstResult.getTitle(), "The Dark Knight");
+        assertEquals(secondResult.getTitle(), "The Dark Knight Rises");
     }
 
 
