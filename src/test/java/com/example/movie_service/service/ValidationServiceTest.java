d2eea784d03629bb120b8a526ff0679f2bd7add0
Huanjia Liang <huanjial@uci.edu>
2024-08-19 22:41:42 -0700

    Refactor ValidationServiceTest.java, make sure there is assertion in each test, and put the expected value in front of actual value in assertEquals().

--- src/test/java/com/example/movie_service/service/ValidationServiceTest.java
+++ src/test/java/com/example/movie_service/service/ValidationServiceTest.java
@@ -6,25 +6,29 @@ import org.junit.jupiter.api.Test;
 import java.time.Year;
 
 import static com.example.movie_service.constant.MovieConstant.*;
+import static com.example.movie_service.constants.TestConstant.ASC;
+import static com.example.movie_service.constants.TestConstant.DESC;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ValidationServiceTest {
+
+class ValidationServiceTest {
 
     private final ValidationServiceImpl validationService = new ValidationServiceImpl();
 
     @Test
     void validateTitle_ShouldThrowException_WhenTitleIsNull() {
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validateTitle(null));
-        assertEquals(exception.getErrorCode(), MISSING_TITLE_CODE);
-        assertEquals(exception.getErrorMessage(), MISSING_TITLE_MESSAGE);
+        assertEquals(MISSING_TITLE_CODE, exception.getErrorCode());
+        assertEquals(MISSING_TITLE_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
     void validateTitle_ShouldThrowException_WhenTitleIsEmpty() {
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validateTitle(""));
-        assertEquals(exception.getErrorCode(), MISSING_TITLE_CODE);
-        assertEquals(exception.getErrorMessage(), MISSING_TITLE_MESSAGE);
+        assertEquals(MISSING_TITLE_CODE, exception.getErrorCode());
+        assertEquals(MISSING_TITLE_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
@@ -36,66 +40,67 @@ public class ValidationServiceTest {
     void validateReleasedYear_ShouldThrowException_WhenYearIsInTheFuture() {
         String futureYear = String.valueOf(Year.now().getValue() + 1);
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validateReleasedYear(futureYear));
-        assertEquals(exception.getErrorCode(), INVALID_YEAR_CODE);
-        assertEquals(exception.getErrorMessage(), INVALID_YEAR_MESSAGE);
+        assertEquals(INVALID_YEAR_CODE, exception.getErrorCode());
+        assertEquals(INVALID_YEAR_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
     void validateReleasedYear_ShouldNotThrowException_WhenYearIsValid() {
-        validationService.validateReleasedYear(String.valueOf(Year.now().getValue()));
+        assertDoesNotThrow(() ->validationService.validateReleasedYear(String.valueOf(Year.now().getValue())));
     }
 
     @Test
     void validateLimit_ShouldThrowException_WhenLimitIsInvalid() {
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validateLimit(5));
-        assertEquals(exception.getErrorCode(), INVALID_LIMIT_CODE);
-        assertEquals(exception.getErrorMessage(), INVALID_LIMIT_MESSAGE);
+        assertEquals(INVALID_LIMIT_CODE, exception.getErrorCode());
+        assertEquals(INVALID_LIMIT_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
     void validateLimit_ShouldNotThrowException_WhenLimitIsValid() {
-        validationService.validateLimit(10);
-        validationService.validateLimit(20);
-        validationService.validateLimit(30);
+        // Parameterized test, also put assert in the test
+        assertDoesNotThrow(()->validationService.validateLimit(10));
+        assertDoesNotThrow(()->validationService.validateLimit(20));
+        assertDoesNotThrow(()->validationService.validateLimit(30));
     }
 
     @Test
     void validatePage_ShouldThrowException_WhenPageIsNegative() {
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validatePage(-1));
-        assertEquals(exception.getErrorCode(), INVALID_PAGE_CODE);
-        assertEquals(exception.getErrorMessage(), INVALID_PAGE_MESSAGE);
+        assertEquals(INVALID_PAGE_CODE, exception.getErrorCode());
+        assertEquals(INVALID_PAGE_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
     void validatePage_ShouldNotThrowException_WhenPageIsValid() {
-        validationService.validatePage(0);
-        validationService.validatePage(1);
+        assertDoesNotThrow(() -> validationService.validatePage(0));
+        assertDoesNotThrow(() -> validationService.validatePage(1));
     }
 
     @Test
     void validateOrderBy_ShouldThrowException_WhenOrderByIsInvalid() {
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validateOrderBy("time"));
-        assertEquals(exception.getErrorCode(), INVALID_ORDER_BY_CODE);
-        assertEquals(exception.getErrorMessage(), INVALID_ORDER_BY_MESSAGE);
+        assertEquals(INVALID_ORDER_BY_CODE, exception.getErrorCode());
+        assertEquals(INVALID_ORDER_BY_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
     void validateOrderBy_ShouldNotThrowException_WhenOrderByIsValid() {
-        validationService.validateOrderBy("rating");
-        validationService.validateOrderBy("title");
-        validationService.validateOrderBy("releaseTime");
+        assertDoesNotThrow(() -> validationService.validateOrderBy("rating"));
+        assertDoesNotThrow(() -> validationService.validateOrderBy("title"));
+        assertDoesNotThrow(() -> validationService.validateOrderBy("releaseTime"));
     }
 
     @Test
     void validateDirection_ShouldThrowException_WhenDirectionIsInvalid() {
         ValidationException exception = assertThrows(ValidationException.class, () -> validationService.validateDirection("north"));
-        assertEquals(exception.getErrorCode(), INVALID_DIRECTION_CODE);
-        assertEquals(exception.getErrorMessage(), INVALID_DIRECTION_MESSAGE);
+        assertEquals(INVALID_DIRECTION_CODE, exception.getErrorCode());
+        assertEquals(INVALID_DIRECTION_MESSAGE, exception.getErrorMessage());
     }
 
     @Test
     void validateDirection_ShouldNotThrowException_WhenDirectionIsValid() {
-        validationService.validateDirection("asc");
-        validationService.validateDirection("desc");
+        assertDoesNotThrow(() -> validationService.validateDirection(ASC));
+        assertDoesNotThrow(() -> validationService.validateDirection(DESC));
     }
 }
