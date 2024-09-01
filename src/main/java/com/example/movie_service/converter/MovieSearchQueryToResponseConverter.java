11d36d7031f3301b04a3d588dadc9493746fa50a
Huanjia Liang <huanjial@uci.edu>
2024-09-01 01:34:06 -0700

    1 of N: Create a DTO to only map data I want to return in the response of search movie endpoint: Created a DTO and a converter.

---
+++ src/main/java/com/example/movie_service/converter/MovieSearchQueryToResponseConverter.java
@@ -0,0 +1,18 @@
+package com.example.movie_service.converter;
+
+import com.example.movie_service.dto.MovieSearchQueryDTO;
+import com.example.movie_service.dto.MovieSearchResponseDTO;
+import org.springframework.core.convert.converter.Converter;
+
+public class MovieSearchQueryToResponseConverter implements Converter<MovieSearchQueryDTO, MovieSearchResponseDTO> {
+    @Override
+    public MovieSearchResponseDTO convert(MovieSearchQueryDTO source) {
+        return new MovieSearchResponseDTO(
+                source.getId(),
+                source.getTitle(),
+                source.getReleaseTime(),
+                source.getPosterPath() != null ? source.getPosterPath() : "default-poster-path",
+                source.getRating() != null ? source.getRating() : 0.0
+        );
+    }
+}
